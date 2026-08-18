07e4dd8c6700a476d12585a62d0d925c7c561971
paynuj1099 <rolandojrremolacio@gmail.com>
2026-08-18 14:38:39 +0800

    rewrite URL

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/__/auth/:path*',
+        destination:
+          'https://project-5e5e4c2f-c4a6-4e44-b7e.firebaseapp.com/__/auth/:path*',
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
